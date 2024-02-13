c98d9f04bd29ee14734832d8b26bbc2ff996026a
fjaoief <darthgun@naver.com>
2024-02-14 01:24:48 +0900

    vscode 용 기초 설정
    샘플 엔티티 추가

---
+++ Assets/1.Scripts/DOTS/Authoring(baker)/SampleUnitAuthoring.cs
@@ -0,0 +1,19 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class SampleUnitAuthoring : MonoBehaviour
+{
+    public class SampleUnitAuthoringBaker : Baker<SampleUnitAuthoring>
+    {
+        public override void Bake(SampleUnitAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent(entity, new SampleUnitComponentData());
+            
+        }
+    }
+}
+
+public struct SampleUnitComponentData : IComponentData
+{
+}
