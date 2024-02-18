e239aecd3d7002f662615a1e3001554eb11f12a8
sdr04021 <43779092+sdr04021@users.noreply.github.com>
2024-02-18 22:35:23 +0900

    RuleSystem, FindDestIndexJob 수정

--- Assets/1.Scripts/DOTS/System/Jobs/FindDestIndexJob.cs
+++ Assets/1.Scripts/DOTS/System/Jobs/FindDestIndexJob.cs
@@ -4,17 +4,20 @@ using Unity.Collections;
 using _1.Scripts.DOTS.Authoring_baker_;
 using Unity.Mathematics;
 using System.Collections.Generic;
+using _1.Scripts.DOTS.Components___Tags;
 
 namespace _1.Scripts.DOTS.System.Jobs
 {
     [BurstCompile]
+    [WithOptions(EntityQueryOptions.IgnoreComponentEnabledState)]
     public partial struct FindDestIndexJob : IJobEntity
     {
         [ReadOnly] public NativeArray<SampleUnitComponentData> SampleUnits;
         [ReadOnly] public MapMakerComponentData MapMaker;
 
-        public void Execute(ref SampleUnitComponentData sampleUnit)
+        public void Execute(ref SampleUnitComponentData sampleUnit, EnabledRefRW<MovingTag> movingTag, EnabledRefRW<LazyTag> lazyTag)
         {
+            // 유닛들이 담긴 배열을 복사하고 복사한 배열 속 유닛들을 현재 유닛에서 가까운 순으로 정렬
             NativeArray<SampleUnitComponentData> sampleUnitsInOrder = new(SampleUnits.Length, Allocator.Temp);
             SampleUnits.CopyTo(sampleUnitsInOrder);
             sampleUnitsInOrder.Sort(new SampleUnitIdexComp{ OriginIndex = sampleUnit.index });
@@ -25,12 +28,18 @@ namespace _1.Scripts.DOTS.System.Jobs
                     sampleUnit.destIndex = sampleUnitsInOrder[i].index;
                     int dx = (int)math.sign(sampleUnitsInOrder[i].index.x - sampleUnit.index.x);
                     int dy =  (int)math.sign(sampleUnitsInOrder[i].index.y - sampleUnit.index.y);
-                    sampleUnit.destIndex = sampleUnit.index + new int2(dx, dy);
+                    sampleUnit.destIndex += new int2(dx, dy);
                     break;
                 }
             }
-
-            sampleUnitsInOrder.Dispose();
+            
+            //목적지를 찾았을 경우 moving tag 활성, 움직일 곳이 없으면 Lazy
+            if(!sampleUnit.index.Equals(sampleUnit.destIndex)){
+                movingTag.ValueRW = true;
+                if(lazyTag.ValueRO) lazyTag.ValueRW = false;
+            }
+            else
+                lazyTag.ValueRW = !lazyTag.ValueRW;
         }
     }
 
--- Assets/1.Scripts/DOTS/System/RuleSystem.cs
+++ Assets/1.Scripts/DOTS/System/RuleSystem.cs
@@ -5,11 +5,15 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using _1.Scripts.DOTS.Components___Tags;
 
 namespace _1.Scripts.DOTS.System
 {
     public partial struct RuleSystem : ISystem
     {
+        EntityQuery behaviorTagQuery;
+        EntityQuery unitQuery;
+    
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
@@ -18,26 +22,36 @@ namespace _1.Scripts.DOTS.System
             var queryMask = query.GetEntityQueryMask();
             //블확실함. 나중에 시간 들여서 확실한 시스템 순서 정해야함. 성능적으로 문제 없으면 업데이트문에 던져버리면 됨
             state.RequireForUpdate(query);
-            */ 
+            */
+            state.RequireForUpdate<MapMakerComponentData>();
+            behaviorTagQuery = new EntityQueryBuilder(Allocator.Temp).WithAny<AttackTag, MovingTag, LazyTag>().Build(ref state);
+            unitQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<SampleUnitComponentData>().Build(ref state);
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
-  //          int x = 0;
             var dt = SystemAPI.Time.DeltaTime;
-            EntityQuery unitQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<SampleUnitComponentData>().Build(ref state);
-            NativeArray<SampleUnitComponentData> sampleUnits = unitQuery.ToComponentDataArray<SampleUnitComponentData>(Allocator.TempJob);
-            MapMakerComponentData mapMaker = SystemAPI.GetSingleton<MapMakerComponentData>();
-            FindDestIndexJob findDestIndexJob = new(){
-                MapMaker = mapMaker,
-                SampleUnits = sampleUnits,
-            };
-            findDestIndexJob.ScheduleParallel();
-            state.Dependency.Complete();
-            sampleUnits.Dispose();
 
+            //Attack Tag, Moving Tag, Lazy Tag 중 하나라도 가진 엔티티가 없을 경우
+            if(behaviorTagQuery.IsEmpty){
+                //행동을 결정해야 함(공격할 타겟 찾기 or 이동할 위치 찾기)
 
+                NativeArray<SampleUnitComponentData> sampleUnits = unitQuery.ToComponentDataArray<SampleUnitComponentData>(Allocator.TempJob);
+                MapMakerComponentData mapMaker = SystemAPI.GetSingleton<MapMakerComponentData>();
+                FindDestIndexJob findDestIndexJob = new(){
+                    MapMaker = mapMaker,
+                    SampleUnits = sampleUnits,
+                };
+                findDestIndexJob.ScheduleParallel();
+                state.Dependency.Complete();
+                sampleUnits.Dispose();
+            }
+            else{
+
+            }
+
+  //          int x = 0;
             // foreach (var (transform, sampleUnit) in SystemAPI.Query<RefRW<LocalTransform>,RefRW<SampleUnitComponentData>>())
              {
                  //각 엔티티의 길찾기 알고리즘은 여기에
