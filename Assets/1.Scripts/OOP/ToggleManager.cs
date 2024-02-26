04ac0d04380a3089a898bfb1b79828de31ea34dc
dnwls6102 <dnwls6102@naver.com>
2024-02-26 21:23:52 +0900

    ToggleManager의 toggleValue값을 spawnerEntity의 컴포넌트 WhattoSpawn의 ToggleValue로 전달하는 기능 구현

--- Assets/1.Scripts/OOP/ToggleManager.cs
+++ Assets/1.Scripts/OOP/ToggleManager.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using Unity.Entities;
+using _1.Scripts.DOTS.Authoring_baker_; //SampleSpawnData 컴포넌트를 불러오기 위함
 
 public class ToggleManager : MonoBehaviour
 {
     public int toggleValue = 0;
     public List<Toggle> toggleButtons = new List<Toggle>();
+    private Entity _spawnerEntity; // World의 Spawner를 담기 위한 변수
 
+    private EntityManager _entityManager;
     private void Start()
     {
         foreach (Toggle toggle in toggleButtons)
@@ -46,5 +50,14 @@ public class ToggleManager : MonoBehaviour
                 toggleValue = 0;
             }
         }
+        _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager; //Mono로 EntityManager를 가져옴
+        //Spawner 엔티티를 가져옴.
+        _spawnerEntity = _entityManager.CreateEntityQuery(typeof(WhattoSpawn)).GetSingletonEntity();
+        //SampleSpawner의 ToggleValue값을 ToggleManager의 업데이트된 toggleValue로 설정
+        _entityManager.SetComponentData<WhattoSpawn>(_spawnerEntity, new WhattoSpawn { ToggleValue = toggleValue });
+        //디버그용 
+        var test = _entityManager.GetComponentData<WhattoSpawn>(_spawnerEntity).ToggleValue;
+        Debug.Log($"{test.ToString()}");
+
     }
 }
