036a6044b83d7e16c11e2a26dfba90ac3bf2d599
dnwls6102 <dnwls6102@naver.com>
2024-02-27 15:24:32 +0900

    ToggleSpawnSystem 추가

---
+++ Assets/1.Scripts/DOTS/System/ToggleSpawnSystem.cs
@@ -0,0 +1,41 @@
+using _1.Scripts.DOTS.Authoring_baker_;
+using _1.Scripts.DOTS.Components___Tags;
+using Unity.Burst;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace _1.Scripts.DOTS.System
+{
+    public partial struct ToggleSpawnSystem : ISystem
+    {
+        [BurstCompile]
+
+        public void Oncreate(ref Systemstate state)
+        {
+
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+
+        }
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+
+        }
+    }
+
+
+
+
+
+
+
+}
