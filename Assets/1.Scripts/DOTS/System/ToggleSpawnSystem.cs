21b10f1c6dae175f57f7d72ecb79e3d4297752ec
dnwls6102 <dnwls6102@naver.com>
2024-02-28 15:19:48 +0900

    맵 타일을 누르면 그 줄에 유닛이 생성되게 하는 기능 추가

--- Assets/1.Scripts/DOTS/System/ToggleSpawnSystem.cs
+++ Assets/1.Scripts/DOTS/System/ToggleSpawnSystem.cs
@@ -3,6 +3,7 @@ using _1.Scripts.DOTS.Components___Tags;
 using Unity.Burst;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 using Unity.Entities;
 using Unity.Mathematics;
@@ -12,16 +13,24 @@ namespace _1.Scripts.DOTS.System
 {
     public partial struct ToggleSpawnSystem : ISystem
     {
+        EntityQuery TileQuery;
         [BurstCompile]
 
-        public void Oncreate(ref Systemstate state)
+        public void Oncreate(ref SystemState state)
         {
-
+            state.RequireForUpdate<SampleSpawnData>();
+            TileQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<MapTileAuthoringComponentData>().Build(ref state);
         }
 
         [BurstCompile]
+
         public void OnUpdate(ref SystemState state)
         {
+            // if (Input.GetMouseButtonDown(0))
+            // {
+            //     Debug.Log("클릭");
+            // }
+            //var SampleSpawner = SystemAPI.GetSingleton<WhattoSpawn>();
 
         }
 
