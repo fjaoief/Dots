e2dfac1dd9f756e55b84208a720b018b66851dd7
fjaoief <darthgun@naver.com>
2024-03-09 01:21:03 +0900

    애니메이션 구현-스프라이트 시트 문제 있음

--- Assets/1.Scripts/DOTS/System/SampleMoveAnimationSystem.cs
+++ Assets/1.Scripts/DOTS/System/SampleMoveAnimationSystem.cs
@@ -1,4 +1,6 @@
-﻿using NSprites;
+﻿using _1.Scripts.DOTS.Components___Tags;
+using _1.Scripts.DOTS.System.Jobs;
+using NSprites;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
@@ -9,16 +11,41 @@ namespace _1.Scripts.DOTS.System
     [UpdateBefore(typeof(SpriteUVAnimationSystem))]
     public partial struct SampleMoveAnimationSystem : ISystem
     {
+        private struct SystemData : IComponentData
+        {
+            public EntityQuery MovableQuery;
+        }
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
-            
+            var systemData = new SystemData();
+            var queryBuilder = new EntityQueryBuilder(Allocator.Temp)
+                .WithAll<MovingTag>()
+                .WithAspect<AnimatorAspect>()
+                .WithOptions(EntityQueryOptions.IgnoreComponentEnabledState);
+            var movableQuery = state.GetEntityQuery(queryBuilder);
+            movableQuery.AddChangedVersionFilter(ComponentType.ReadOnly<MovingTag>());
+            systemData.MovableQuery = movableQuery;
+
+            _ = state.EntityManager.AddComponentData(state.SystemHandle, systemData);
+
+            queryBuilder.Dispose();
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
+            var systemData = SystemAPI.GetComponent<SystemData>(state.SystemHandle);
+            if (!SystemAPI.TryGetSingleton<AnimationSettings>(out var animationSettings))
+                return;
+            var time = SystemAPI.Time.ElapsedTime;
 
+            var animationSwitchJob = new AnimationJob
+            {
+                AnimationSettings = animationSettings,
+                Time = time
+            };
+            state.Dependency = animationSwitchJob.ScheduleParallelByRef(systemData.MovableQuery, state.Dependency);
         }
 
         [BurstCompile]
